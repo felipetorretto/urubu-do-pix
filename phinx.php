22a5753d64feee362ed0aa2778d8df32b75357bb
Felipe Torretto <felipetorretto@gmail.com>
2024-04-14 16:22:14 -0300

    chore: config Phinx

---
+++ phinx.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'migrations',
+        'default_environment' => 'development',
+        'production' => [
+            'adapter' => 'mysql',
+            'host' => 'db',
+            'name' => 'urubu',
+            'user' => 'root',
+            'pass' => 'root',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ],
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => 'db',
+            'name' => 'urubu',
+            'user' => 'root',
+            'pass' => 'root',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ],
+        'testing' => [
+            'adapter' => 'mysql',
+            'host' => 'db',
+            'name' => 'urubu',
+            'user' => 'root',
+            'pass' => 'root',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ]
+    ],
+    'version_order' => 'creation'
+];
